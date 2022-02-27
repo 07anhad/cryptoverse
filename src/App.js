8751182abb94409cb150fe7c744c735a924eb293
Anhad <84476084+07anhad@users.noreply.github.com>
2022-02-27 23:40:48 +0530

    material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import { BrowserRouter } from 'react-router-dom';
+import { colors, makeStyles } from '@material-ui/core';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import Header  from './Components/Header';
+import CoinPage from './Pages/CoinPage';
+import Homepage from './Pages/Homepage';
 
 function App() {
+
+  const useStyles = makeStyles(() => ({
+    App: {
+      backgroundColor: '#14161a',
+      color: '#fff',
+      minHeight: '100vh'
+    }
+  }));
+
+  const classes = useStyles();
+
   return (
     <BrowserRouter>
-      <Header></Header>
+      <div className={classes.App}>
+        <Header />
+        <Route path='/' component={Homepage} exact/>
+        <Route path='/coins/:id' component={ CoinPage}/>
+      </div>
     </BrowserRouter>
   );
 }
